05399d2553dc5db0bb86da6eb43b2db70ac7c73c
yakubovich.vi <yakuboshka93@mail.ru>
2021-02-15 10:09:52 +0200

    Added 'export to csv'

--- vacancy/admin.py
+++ vacancy/admin.py
@@ -2,7 +2,32 @@ from django.contrib import admin
 
 from .forms import ProductForm
 from .models import Product
-# Register your models here.
+import csv
+import datetime
+from django.http import HttpResponse
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;' \
+                                      'filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -11,3 +36,4 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ('company',)
     ordering = ('date',)
     form = ProductForm
+    actions = [export_to_csv]
